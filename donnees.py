06355d9d130c527423bb4bdc44fc7ba85bf8654c
kalliste14 <mailtest@bbox.fr>
2019-05-16 17:55:51 +0200

    Ajout du fichier des mots du jeu

---
+++ donnees.py
@@ -0,0 +1,5 @@
+# Nombre de chances autorisées
+nb_Chances = 8
+
+# Liste de mots
+liste_Mots = ("Abricot","Ananas","Chataigne","Lunette","Fichier","Sommeil","Chanceux","Betisier","Compote","Basket","Amoureux","Desespoir","Maladie","Noisette")
